f9587adc7e4d880c8f4f35fc65a6f114c196fcc7
Himani Chaudhary <himanichaudhary@Himanis-MacBook-Air.local>
2023-01-22 13:59:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage/Homepage";
+import LeftMenu from "./components/LeftMenu/LeftMenu";
+import RightMenu from "./components/RightMenu/RightMenu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Homepage />
+      {/*<LeftMenu/>
+      <RightMenu/>*/}
     </div>
   );
 }
